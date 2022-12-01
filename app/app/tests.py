d2e07c58389414e43b8727a7228e32f8ce24ee79
hana.adm <hanakamer@gmail.com>
2022-12-01 10:02:08 +0100

    linter fix

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,18 +6,19 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_number(self):
         """Test addinf numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
 
-        res = calc.subtract(10,7)
+        res = calc.subtract(10, 7)
 
         self.assertEqual(res, 3)
